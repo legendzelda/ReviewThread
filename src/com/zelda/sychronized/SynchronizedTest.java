5d5949f3dd34bedcaf2cd701c5c26ad33a2b2316
sinitek_pc <490486873@qq.com>
2017-11-22 19:01:06 +0800

    第一次

---
+++ src/com/zelda/sychronized/SynchronizedTest.java
@@ -0,0 +1,41 @@
+package com.zelda.sychronized;
+
+/**
+ * @author bu.han
+ */
+    public class SynchronizedTest {
+
+    public static int num = 100;
+
+    public static void main(String[] args) {
+        Runner r = new Runner();
+        Thread t1 = new Thread(r);
+        Thread t2 = new Thread(r);
+        t1.setName("name1 ");
+        t2.setName("name2 ");
+        t1.start();t2.start();
+    }
+
+    static class Runner implements Runnable {
+
+        @Override
+        public  void run() {
+            while (num > 0) {
+                try {
+                    num -- ;
+                    Thread.sleep(100);
+                    System.out.println(Thread.currentThread().getName()+num);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+    }
+}
+
+
+
+
+
